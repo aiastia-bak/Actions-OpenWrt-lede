07e868dbfc0b1f4cc5d1174a305febd4635110bf
未来 <aiastia@users.noreply.github.com>
2021-08-08 15:42:58 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -17,4 +17,6 @@ git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git  package/le
 git clone https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
 git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/luci-app-jd-dailybonus
 git clone -b lede https://github.com/pymumu/luci-app-smartdns.git  package/lean/luci-app-smartdns
+git clone https://github.com/vernesong/OpenClash.git package/luci-app-openclash
+
 #git clone https://github.com/xiaorouji/openwrt-passwall package/openwrt-passwall
